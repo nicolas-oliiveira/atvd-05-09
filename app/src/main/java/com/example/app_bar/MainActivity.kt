3b06f1c5b933c5a2393b8b78ced40be0065c6488
nicolas-oliiveira <nicolas.hassan20@gmail.com>
2023-09-06 08:44:47 -0300

    editado

--- app/src/main/java/com/example/app_bar/MainActivity.kt
+++ app/src/main/java/com/example/app_bar/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.app_bar
 
 import android.app.DatePickerDialog
+import android.app.Dialog
 import android.os.Bundle
 import android.widget.DatePicker
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,11 +16,15 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -28,6 +36,7 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -42,6 +51,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -53,6 +63,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -61,16 +72,26 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.app_bar.ui.theme.App_barTheme
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -161,15 +182,15 @@ fun SimpleTopAppBar() {
 
 
     //Button
-    @Composable
-    fun ElevatedButtonSample() {
-        ElevatedButton(onClick = { /* Do something! */ }) { Text("Cadastrar") }
-    }
+@Composable
+ fun ElevatedButtonSample() {
+     ElevatedButton(onClick = { /* Do something! */ }) { Text("Cadastrar") }
+ }
 
-    @Composable
-    fun ElevatedButtonSample2() {
-        ElevatedButton(onClick = { /* Do something! */ }) { Text("Cancelar") }
-    }
+@Composable
+ fun ElevatedButtonSample2() {
+     ElevatedButton(onClick = { /* Do something! */ }) { Text("Cancelar") }
+ }
 
 
 // original do projeto
@@ -182,14 +203,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    App_barTheme {
-        Greeting("Android")
 
-    }
-}
 }
 
 
@@ -200,10 +214,8 @@ fun CamposTexto() {
 
     val nome = remember { mutableStateOf("") }
     val telefone = remember { mutableStateOf("") }
-    val origem = remember { mutableStateOf("") }
-    val dataDeContato = remember { mutableStateOf("") }
-    val observacao = remember { mutableStateOf("") }
-    val estado = remember { mutableStateOf("") }
+    val text = remember { mutableStateOf("") }
+
 
     Column(modifier = Modifier.padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally) {
         /*var nome by remember { mutableStateOf("") }
@@ -241,20 +253,55 @@ fun CamposTexto() {
 
         EditableExposedDropdownMenuSample()
 
-        OutlinedTextField(
-            value = dataDeContato.value,
-            onValueChange = { dataDeContato.value = it },
-            label = { Text("Data de Contato") },
+        //Data de contato
+        val context = LocalContext.current
+        val calendar = Calendar.getInstance()
+        var selectedDateText by remember { mutableStateOf("") }
+// Fetching current year, month and day
+        val year = calendar[Calendar.YEAR]
+        val month = calendar[Calendar.MONTH]
+        val dayOfMonth = calendar[Calendar.DAY_OF_MONTH]
+        val datePicker = DatePickerDialog(
+            context,
+            { _: DatePicker, selectedYear: Int, selectedMonth: Int, selectedDayOfMonth: Int ->
+                selectedDateText = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
+            }, year, month, dayOfMonth
+        )
+        datePicker.datePicker.minDate = calendar.timeInMillis
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                onClick = {
+                    datePicker.show()
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .background(Color.White)
+                    .clip(shape = CircleShape)
+            ) {
+                Text(
+                    text = if (selectedDateText.isNotEmpty()) {
+                        "Data Selecionada $selectedDateText"
+                    } else {
+                        "Data de Contato"
+                    }
+                )
+            }
+        }
+
+        TextField(
+            value = text.value ,
+            onValueChange = { text.value = it },
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(bottom = 16.dp)
-        )
-
-        OutlinedTextField(
-            value = observacao.value,
-            onValueChange = { observacao.value = it },
+                .height(100.dp)
+                .padding(10.dp)
+                .border(width = 1.dp, color = Color.Black, shape = RoundedCornerShape(8.dp)),
             label = { Text("Observação") },
-            modifier = Modifier.fillMaxWidth()
         )
     }
 
@@ -268,21 +315,30 @@ fun EditableExposedDropdownMenuSample() {
     val options = listOf("São Paulo", "Rio de Janeiro", "Bahia", "Amazonas")
     var expanded by remember { mutableStateOf(false) }
     var selectedOptionText by remember { mutableStateOf("") }
+
     ExposedDropdownMenuBox(
         expanded = expanded,
         onExpandedChange = { expanded = !expanded },
-        modifier = Modifier.width(1020.dp),
+        modifier = Modifier
+            .width(1020.dp)
+            .background(Color.White)
+            .clip(shape = CircleShape)
     ) {
         TextField(
-            // The `menuAnchor` modifier must be passed to the text field for correctness.
-            modifier = Modifier.menuAnchor(),
+
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White)
+                .clip(shape = CircleShape)
+                .border(1.dp, Color.Black, CircleShape),
+
             value = selectedOptionText,
             onValueChange = { selectedOptionText = it },
             label = { Text("Origem") },
             trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
             colors = ExposedDropdownMenuDefaults.textFieldColors(Color.Transparent),
         )
-        // filter options based on text field value
+
         val filteringOptions = options.filter { it.contains(selectedOptionText, ignoreCase = true) }
         if (filteringOptions.isNotEmpty()) {
             ExposedDropdownMenu(
@@ -303,6 +359,17 @@ fun EditableExposedDropdownMenuSample() {
         }
     }
 }
+
+
+
+//
+
+
+
+
+
+
+
 
 
 
